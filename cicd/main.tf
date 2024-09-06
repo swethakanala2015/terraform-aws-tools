7b54df07c3a7437c6879dbec9e59fc4330abed5a
swethakanala <swetha.bim@gmail.com>
2024-09-06 14:12:23 -0400

    functions

--- cicd/main.tf
+++ cicd/main.tf
@@ -33,7 +33,7 @@ resource "aws_key_pair" "tools" {
   key_name   = "tools"
   # you can paste the public key directly like this
   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-  public_key = C:\Users\zaree("~/.ssh/tools.pub")
+  public_key = zaree("~/.ssh/tools.pub")
   # ~ means windows home directory
 }
 
