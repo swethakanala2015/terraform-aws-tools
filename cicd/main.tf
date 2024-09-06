a19238ad4dd9c3b4a49a258292ff0c6dbf04e581
swethakanala <swetha.bim@gmail.com>
2024-09-06 10:18:57 -0400

    functions

--- cicd/main.tf
+++ cicd/main.tf
@@ -4,8 +4,8 @@ module "jenkins" {
   name = "jenkins-tf"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0f0f7936b9ceb8156"] #replace your SG
-  subnet_id = "subnet-064fc25efbfdd49e6" #replace your Subnet
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"] #replace your SG
+  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -19,9 +19,9 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0f0f7936b9ceb8156"]
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   # convert StringList to list and get first element
-  subnet_id = "subnet-064fc25efbfdd49e6"
+  subnet_id = "subnet-0ea509ad4cba242d7"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
   tags = {
@@ -29,13 +29,13 @@ module "jenkins_agent" {
   }
 }
 
-# resource "aws_key_pair" "tools" {
-#   key_name   = "tools"
-#   # you can paste the public key directly like this
-#   #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
-#   public_key = file("~/.ssh/tools.pub")
-#   # ~ means windows home directory
-# }
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
 
 module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -43,9 +43,9 @@ module "nexus" {
   name = "nexus"
 
   instance_type          = "t3.medium"
-  vpc_security_group_ids = ["sg-0f0f7936b9ceb8156"]
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   # convert StringList to list and get first element
-  subnet_id = "subnet-064fc25efbfdd49e6"
+  subnet_id = "subnet-0ea509ad4cba242d7"
   ami = data.aws_ami.nexus_ami_info.id
   key_name = aws_key_pair.tools.key_name
   root_block_device = [
